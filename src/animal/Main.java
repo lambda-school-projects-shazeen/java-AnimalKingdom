0503d1cc218be0ac98b75c681e0b8b37fbea20bb
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-03-09 13:32:00 -0800

    Main class

---
+++ src/animal/Main.java
@@ -0,0 +1,16 @@
+package animal;
+/**
+javac animal/*.java
+jar cvfe animal.jar animal.Main animal/*.class
+java -jar animal.jar
+*/
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Homework 2");
+	}
+}
